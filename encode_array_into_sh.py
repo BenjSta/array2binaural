c33a7e2f8000dc75db66894e427e8233f7290b8b
Benjamin Stahl <benjamin.stahl@kug.ac.at>
2023-11-17 11:58:04 +0100

    refactoring

--- encode_array_into_sh.py
+++ encode_array_into_sh.py
@@ -9,10 +9,11 @@ FS = 32000
 
 f = h5py.File(ARRAY_IR_FILE_PATH, "r")
 
-## load impulse responses
+# load impulse responses
 fs = int(list(f["SamplingFreq_Hz"])[0][0])
 # resample to FS
-ir = scipy.signal.resample_poly(np.array(f["IR"])[:, :, [0, 1, 2, 4, 5]], FS, fs)
+ir = scipy.signal.resample_poly(
+    np.array(f["IR"])[:, :, [0, 1, 2, 4, 5]], FS, fs)
 zenith = np.array(f["Theta"])  # zenith angle in radians [1 x nDirection]
 zenith = zenith[0, :]
 azi = np.array(f["Phi"])  # azimuth in radians [1 x nDirection]
@@ -29,10 +30,10 @@ REGUL_DIAG_LOAD = 0.1
 e = np.linalg.eigvalsh(Y_gr.T @ weights @ Y_gr)
 Y_gr_pinv = (
     np.linalg.inv(
-        Y_gr.T @ weights @ Y_gr + np.max(e) * REGUL_DIAG_LOAD * np.eye(Y_gr.shape[1])
+        Y_gr.T @ weights @ Y_gr +
+        np.max(e) * REGUL_DIAG_LOAD * np.eye(Y_gr.shape[1])
     )
-    @ Y_gr.T
-    @ weights
+    @ Y_gr.T @ weights
 )
 hoa_array = Y_gr_pinv @ ir
 
@@ -46,7 +47,8 @@ if False:
     shmat = spaudiopy.sph.sh_matrix(TRUNC_ORDER, azigr, zengr, "real")
     MIC = 1
 
-    b, a = scipy.signal.butter(2, (FMIN_BANDPASS, FMAX_BANDPASS), "bandpass", fs=FS)
+    b, a = scipy.signal.butter(
+        2, (FMIN_BANDPASS, FMAX_BANDPASS), "bandpass", fs=FS)
     VMIN = -60
     VMAX = -30
 
@@ -85,7 +87,8 @@ if False:
         s=10,
         c=10
         * np.log10(
-            np.mean(scipy.signal.lfilter(b, a, ir[:, :, MIC], axis=0) ** 2, axis=0)
+            np.mean(scipy.signal.lfilter(
+                b, a, ir[:, :, MIC], axis=0) ** 2, axis=0)
         ),
         vmin=VMIN,
         vmax=VMAX,
@@ -95,4 +98,5 @@ if False:
     plt.grid(True)
     plt.show()
 
-np.save("Easycom_array_%dHz_o%d_22samps_delay.npy" % (FS, INTERP_ORDER), hoa_array)
+np.save("Easycom_array_%dHz_o%d_22samps_delay.npy" %
+        (FS, INTERP_ORDER), hoa_array)
